00c36fb59f5cb8cb89ec31097f08d4c75230ce46
Suhas More <suhas.more@joshsoftware.com>
2020-11-17 18:05:47 +0530

    updated test

--- src/components/ProductDetails/index.jsx
+++ src/components/ProductDetails/index.jsx
@@ -1,5 +1,7 @@
-import { Rate, Spin, Skeleton, Row, Col } from 'antd';
 import React, { useEffect, useState } from 'react';
+import {
+  Rate, Spin, Skeleton, Row, Col,
+} from 'antd';
 import { useParams, withRouter } from 'react-router-dom';
 import Client from '../../service/DummyClient';
 import { Img, AddToCart, Buy } from '../../common';
@@ -31,6 +33,8 @@ const ProductDetails = ({ history: { push } }) => {
       });
     });
   }, []);
+
+  // DUMMY description of profuct
   const getDesc = () => PDESC.map(item => <li>{item}</li>);
   const {
     loading,
@@ -60,8 +64,10 @@ const ProductDetails = ({ history: { push } }) => {
                 <p>{desc}</p>
               </div>
               <div className="rating">
-                <Rate defaultValue={getRatings()} allowHalf />(
-                {`${getVots()} votes`})
+                <Rate defaultValue={getRatings()} allowHalf />
+                (
+                {`${getVots()} votes`}
+                )
               </div>
             </div>
             <div className="details">
